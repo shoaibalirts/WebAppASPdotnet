7587fcfbca148199afc6f61deb68376f604d876f
shoaibalirts <shoaibali.rts@gmail.com>
2025-05-23 11:50:20 +0200

    played around query string

--- FirstWebAppASPdotnet/Program.cs
+++ FirstWebAppASPdotnet/Program.cs
@@ -5,13 +5,45 @@ var app = builder.Build();
 //app.MapGet("/", () => "Hello World123!");
 app.Run(async (HttpContext context) =>
 {
+    /*
     context.Response.StatusCode = 200;
     context.Response.Headers["MyName"] = "Shoaib Ali";
     context.Response.Headers["Date"] = "23-05-2026";
+    */
+    /*
+    string path = context.Request.Path;
+    string method = context.Request.Method;
+    context.Response.Headers["Content-Type"] = "text/html";
+    if (path == "/")
+    {
+        await context.Response.WriteAsync($"<p>Root</p>");
+    }
+    else if (path == "/home")
+    {
+        await context.Response.WriteAsync($"<p>Root</p>");
+        await context.Response.WriteAsync($"<p>{method}</p>");
 
-    await context.Response.WriteAsync("Hello");
-    await context.Response.WriteAsync("World");
+    }
+    else if (path == "/about")
+    {
+        await context.Response.WriteAsync($"<li>About me</li>");
+    }
+    else
+    {
+        await context.Response.WriteAsync($"<p>{path}</p>");
 
+    }
+    */
+    context.Response.Headers["Content-type"] = "text/html";
+    if (context.Request.Method == "GET")
+    {
+        if (context.Request.Query.ContainsKey("id"))// Query is a dictionary
+        {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p>{id}</p>");
+        }
+
+    }
 });
 
 app.Run();
