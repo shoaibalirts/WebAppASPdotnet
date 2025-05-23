e4758a3fa4bd1f10182fdd5c5fdf5a68a97cae74
shoaibalirts <shoaibali.rts@gmail.com>
2025-05-23 09:13:58 +0200

    Resolved merge conflicts

--- FirstWebAppASPdotnet/Program.cs
+++ FirstWebAppASPdotnet/Program.cs
@@ -2,10 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-<<<<<<< HEAD
-app.MapGet("/", () => "Hello Worldxxcdxc123!");
-=======
 app.MapGet("/", () => "Hello World123!");
->>>>>>> 147de4bb9fc7cb5e58184770a7df600c312ca313
+
 
 app.Run();
