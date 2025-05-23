024d6ed6b20399217a723e3aba358a3f68f7fa7f
shoaibalirts <shoaibali.rts@gmail.com>
2025-05-23 09:46:38 +0200

    Response headers (disctionay which is a key value pair) is manipulated

--- FirstWebAppASPdotnet/Program.cs
+++ FirstWebAppASPdotnet/Program.cs
@@ -2,7 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World123!");
+//app.MapGet("/", () => "Hello World123!");
+app.Run(async (HttpContext context) =>
+{
+    context.Response.StatusCode = 200;
+    context.Response.Headers["MyName"] = "Shoaib Ali";
+    context.Response.Headers["Date"] = "23-05-2026";
 
+    await context.Response.WriteAsync("Hello");
+    await context.Response.WriteAsync("World");
+
+});
 
 app.Run();
