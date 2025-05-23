0ea427e34517f168c787533a8e5a76b54cd52a4f
shoaibalirts <shoaibali.rts@gmail.com>
2025-05-23 09:00:42 +0200

    test

--- FirstWebAppASPdotnet/Program.cs
+++ FirstWebAppASPdotnet/Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Worldxxcdxc!");
+app.MapGet("/", () => "Hello Worldxxcdxc123!");
 
 app.Run();
